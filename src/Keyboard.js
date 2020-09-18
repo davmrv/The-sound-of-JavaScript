5adf4071d8c728abb200a2dd182fe5efc5a60094
David Maravilla <dmaravilla@applaudostudios.com>
2020-09-17 20:41:56 -0600

    Add midi support

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -7,82 +7,122 @@ export default class Keyboard {
         frequency: 523.25,
         octave: 5,
         key: 'a',
+        midiKey: 36,
       },
       {
         note: 'CS',
         frequency: 554.37,
         octave: 5,
         key: 'w',
+        midiKey: 37,
       },
       {
         note: 'D',
         frequency: 587.33,
         octave: 5,
         key: 's',
+        midiKey: 38,
       },
       {
         note: 'DS',
         frequency: 622.25,
         octave: 5,
         key: 'e',
+        midiKey: 39,
       },
       {
         note: 'E',
         frequency: 659.25,
         octave: 5,
         key: 'd',
+        midiKey: 40,
       },
       {
         note: 'F',
         frequency: 698.46,
         octave: 5,
         key: 'f',
+        midiKey: 41,
       },
       {
         note: 'FS',
         frequency: 739.99,
         octave: 5,
         key: 't',
+        midiKey: 42,
       },
       {
         note: 'G',
         frequency: 783.99,
         octave: 5,
         key: 'g',
+        midiKey: 43,
       },
       {
         note: 'GS',
         frequency: 830.61,
         octave: 5,
         key: 'y',
+        midiKey: 44,
       },
       {
         note: 'A',
         frequency: 880,
         octave: 5,
         key: 'h',
+        midiKey: 45,
       },
       {
         note: 'AS',
         frequency: 932.33,
         octave: 5,
         key: 'u',
+        midiKey: 46,
       },
       {
         note: 'B',
         frequency: 987.77,
         octave: 5,
         key: 'j',
+        midiKey: 47,
       },
     ];
     this.oscList = {};
     this.audioContext = new AudioContext;
     this.waveType = 'sine';
+    this.setupMidi();
 
     this.playNote = this.playNote.bind(this);
     this.stopNote = this.stopNote.bind(this);
     this.playNoteFromKey = this.playNoteFromKey.bind(this);
     this.stopNoteFromKey = this.stopNoteFromKey.bind(this);
+    this.setupMidi = this.setupMidi.bind(this);
+  }
+
+  setupMidi() {
+    navigator.requestMIDIAccess({
+      sysex: true,
+    })
+      .then((access) => {
+        const inputs = access.inputs;
+        const controllerId = Array.from(inputs.keys())[1];
+        const controller = inputs.get(controllerId);
+
+        controller.onmidimessage = (event) => {
+          const midiKeydown = 144;
+          const midiKeyup = 128;
+          const playedNote = this.notesList.find(note => note.midiKey === event.data[1]);
+          const element = document.querySelector(`div[data-frequency="${playedNote.frequency}"`);
+
+          if (event.data[0] === midiKeydown) {
+            this.playNote({ target: element });
+          }
+
+          if (event.data[0] === midiKeyup) {
+            this.stopNote({ target: element });
+          }
+        }
+      });
   }
 
   createKeyboard() {
